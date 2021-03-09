0c4edadcdb1248cd06111ab75dff9dc6c07931e2
Mark <mark.duplock@gmail.com>
2021-03-09 19:30:14 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 def prime_checker(number) :
-    counter = 0
-    for i in range(1, number + 1):
+    is_prime = True
+    for i in range(2, number):
         if number % i == 0:
-            counter += 1
-    if counter > 2:
-        print("It's not a prime number")
-    else:
+            is_prime = False
+        
+    if is_prime:
         print("It's a prime number")
+    else:
+        print("It's not a prime number")
 
 n = int(input("Check this number: "))
 prime_checker(number=n)
